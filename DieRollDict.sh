86b98468cf5883c3bbf12cb426311f9896d3a8f5
poojasri <poojasrijampani@gmail.com>
2020-08-04 18:38:35 +0530

    Assignment Day8

---
+++ DieRollDict.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+declare -A dictionary
+count=0
+while(( $count != 10 ))
+do
+random=$(( $RANDOM%6 + 1 ))
+dict[$random]=$((${dict[$random]} + 1 ))
+count=${dict[$random]};
+done
+echo ${!dict[@]}
+echo ${dict[@]}
+max=${dict[1]}
+min=${dict[1]}
+for((i=1;i<7;i++))
+do
+if [[ ${dict[$i]} -lt $min ]]
+then
+min=${dict[$i]}
+a=$i;
+fi
+if [[ ${dict[$i]} -gt $max ]]
+then
+max=${dict[$i]}
+b=$i;
+fi
+done
+echo "Maximum number of counts are " $b " = " $max "times ";
+echo "Minimum number of counts are " $a " = " $min "times ";
